8eb1868d4839aea0bb8c1b3f56357d81e00740c5
Nikolay37731 <anikolai3773@gmail.com>
2026-01-07 19:14:35 +0100

    Solved problem: Supermarket

---
+++ StacksAndQueues/06_supermarket_LAB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+Queue<string> queue = new Queue<string>();
+string input;
+while ((input = Console.ReadLine()) != "end")
+{
+    if(input == "Paid")
+    {
+        Console.WriteLine(string.Join("\n", queue));
+        queue.Clear();
+    }
+    else
+    {
+        queue.Enqueue(input);
+    }
+}
+
+Console.WriteLine($"{queue.Count} people remaining.");
