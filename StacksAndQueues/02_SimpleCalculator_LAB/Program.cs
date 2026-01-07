89da002809f9c1c0915359856c6547b1a521c500
Nikolay37731 <anikolai3773@gmail.com>
2026-01-07 17:48:02 +0100

    Solved task: Simple calculator

--- StacksAndQueues/02_SimpleCalculator_LAB/Program.cs
+++ StacksAndQueues/02_SimpleCalculator_LAB/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] expression = Console.ReadLine().Split(" ").ToArray();
+            Stack<string> tokens = new Stack<string>(expression);
+
+            while (tokens.Any())
+            {
+                int firstNum = int.Parse(tokens.Pop());
+                char operatorr = Convert.ToChar(tokens.Pop());
+                int secondNum = int.Parse(tokens.Pop());
+
+                switch(operatorr)
+                {
+                    case '+':
+                    firstNum += secondNum;
+                    break;
+                    case '-':
+                    firstNum -= secondNum;
+                    break;
+                    default:
+                    break;
+                }
+
+                tokens.Push(firstNum.ToString());
+
+                if (tokens.Count() == 1)
+                {
+                    break;
+                }
+            }   // 1 + 2 + 3 + 4 + 5
+
+            Console.WriteLine(tokens.FirstOrDefault());
+        }
+    }
+    
+}
