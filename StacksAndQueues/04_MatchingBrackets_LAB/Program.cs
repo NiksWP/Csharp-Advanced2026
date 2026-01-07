c5351cf1fa703f5865230bf6d149a40c48010ed8
Nikolay37731 <anikolai3773@gmail.com>
2026-01-07 17:52:43 +0100

    Solved problem: Matching brackets

---
+++ StacksAndQueues/04_MatchingBrackets_LAB/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> indexes = new Stack<int>();
+            Stack<string> brackets = new Stack<string>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char current = expression[i];
+                if(current == '(')
+                {
+                    indexes.Push(i);
+                }
+
+                if (current == ')')
+                {
+                    int startIndex = indexes.Pop();
+                    string bracket = expression.Substring(startIndex, i - startIndex + 1);
+                    brackets.Push(bracket);
+                }
+            }
+            foreach (string bracket in brackets)
+            {
+                Console.WriteLine(bracket);
+            }
+        }
+    }
+}
