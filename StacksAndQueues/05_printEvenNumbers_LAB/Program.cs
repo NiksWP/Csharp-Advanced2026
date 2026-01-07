84ae6a3b53642829067b09320b98ec8a3c41391c
Nikolay37731 <anikolai3773@gmail.com>
2026-01-07 18:24:11 +0100

    Solved problem: Print even numbers

---
+++ StacksAndQueues/05_printEvenNumbers_LAB/Program.cs
@@ -0,0 +1,14 @@
+﻿var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var queue = new Queue<int>();
+var flag = 1;
+
+foreach (var number in numbers)
+{
+    if (flag % 2 == 0)
+    {
+        queue.Enqueue(number);
+    }
+    flag++;
+}
+
+Console.WriteLine(String.Join(", ", queue));
