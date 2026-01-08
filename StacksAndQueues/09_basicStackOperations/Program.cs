090e03f72862b4bc9f388eb96d14f60f645f7c10
Nikolay37731 <anikolai3773@gmail.com>
2026-01-08 15:46:48 +0100

    Solved problem: Basic stack operations

---
+++ StacksAndQueues/09_basicStackOperations/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+var tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var(quantity, toRemove, searched) = (tokens[0], tokens[1], tokens[2]);
+bool flag = false;
+var smallest = int.MaxValue;
+
+
+var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var stack = new Stack<int>(numbers);
+
+RemoveNumbers(stack, toRemove);
+
+
+var arr = stack.ToArray();
+foreach (int num in arr)
+{
+    if (num == searched)
+    {
+        flag = true;
+    }
+    if (num <= smallest)
+    {
+        smallest = num;
+    }
+}
+
+if (flag)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(smallest);
+}
+
+
+static void RemoveNumbers(Stack<int> stack, int numbers)
+{
+    if (stack == null)
+    {
+        throw new NullReferenceException("Stack is null");
+    }
+    if (stack.Count == 0)
+    {
+        throw new InvalidOperationException("Stack is empty");
+    }
+
+    if (stack.Any())
+    {
+        for (int i = 0; i < numbers; i++)
+        {
+            stack.Pop();
+            if (stack.Count() == 0)
+            {
+                Console.WriteLine("o");
+                return;
+            }
+        }
+    }
+}
