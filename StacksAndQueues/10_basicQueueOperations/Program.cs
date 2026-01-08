06cbb9f01e56531f2072d83e296254c1d7ae9132
Nikolay37731 <anikolai3773@gmail.com>
2026-01-08 16:09:32 +0100

    Solved problem: Basic queue operations

---
+++ StacksAndQueues/10_basicQueueOperations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+var tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var(quantity, toRemove, wanted) = (tokens[0], tokens[1], tokens[2]);
+
+// Filling the array with numbers
+var queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+//Removing n numbers with method
+RemoveNumbers(queue, toRemove);
+if (queue.Count == 0)
+{
+    return;
+}
+
+CheckForElement(queue, wanted);
+
+
+static void CheckForElement(Queue<int> queue, int wanted)
+{
+    if (queue.Contains(wanted))
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine(queue.Min());
+    }
+}
+
+
+static void RemoveNumbers(Queue<int> queue, int quantity)
+{
+    if (queue == null)
+    {
+        throw new NullReferenceException("Stack is null");
+    }
+    if (queue.Count == 0)
+    {
+        throw new InvalidOperationException("Stack is empty");
+    }
+
+    for (int i = 0; i < quantity; i++)
+    {
+        queue.Dequeue();
+        if (queue.Count == 0)
+        {
+            Console.WriteLine("0");
+            return;
+        }
+    }
+}
