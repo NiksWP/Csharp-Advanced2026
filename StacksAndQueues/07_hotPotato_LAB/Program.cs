1487b97bedead2316ab4cca1631cd89898308bf9
Nikolay37731 <anikolai3773@gmail.com>
2026-01-08 14:08:59 +0100

    Solved problem: Hot potato

---
+++ StacksAndQueues/07_hotPotato_LAB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+Queue<string> queue = new Queue<string>(Console.ReadLine().Split());
+int tosses = int.Parse(Console.ReadLine());
+
+while (queue.Count() > 1)
+{
+    for (int i = 1; i < tosses; i++)       // niki gosho sasho ico
+    {                                     // gosho sasho ico niki
+        string currentKid = queue.Dequeue();
+        queue.Enqueue(currentKid);
+    }
+
+    Console.WriteLine($"Removed {queue.Dequeue()}");
+}
+
+System.Console.WriteLine($"Last is {queue.FirstOrDefault()}");
