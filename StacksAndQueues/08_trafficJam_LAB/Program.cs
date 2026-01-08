b41167dc311b9943846e76b00e02798bfd1979fa
Nikolay37731 <anikolai3773@gmail.com>
2026-01-08 14:21:26 +0100

    Solved problem: Traffic jam

---
+++ StacksAndQueues/08_trafficJam_LAB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+var capacity = int.Parse(Console.ReadLine());
+var queue = new Queue<string>();
+var carsPassed = 0;
+
+string input;
+while ((input = Console.ReadLine()) != "end")
+{
+    if (input == "green")
+    {
+        for (int i = 0; i < capacity; i++)
+        {
+            if (queue.Count() == 0)
+            {
+               break;
+            }
+
+            Console.WriteLine($"{queue.Dequeue()} passed!");
+            carsPassed++;
+        }
+    }
+    else
+    {
+        queue.Enqueue(input);
+    }
+}
+
+Console.WriteLine($"{carsPassed} cars passed the crossroads.");
