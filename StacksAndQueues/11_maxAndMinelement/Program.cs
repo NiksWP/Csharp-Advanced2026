1063606d6990dc7e0630c9bdc3194ff26b2f8468
Nikolay37731 <anikolai3773@gmail.com>
2026-01-10 22:04:53 +0100

    Solved problem: Min and max element

---
+++ StacksAndQueues/11_maxAndMinelement/Program.cs
@@ -0,0 +1,31 @@
+﻿var stack = new Stack<int>();
+int n = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    if (input[0] == 1)
+    {
+        stack.Push(input[1]);
+    }
+    else if(input[0] == 2 && stack.Any())
+    {
+        stack.Pop();
+    }
+    else if (input[0] == 3 && stack.Any())
+    {
+        Console.WriteLine(stack.Max());
+    }
+    else if (input[0] == 4 && stack.Any())
+    {
+        Console.WriteLine(stack.Min());
+    }
+
+    n--;
+    if (n == 0)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(string.Join(", ", stack));
