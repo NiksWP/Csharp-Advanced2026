f4c04666174a9b5010a8257736cb4853516351a6
Nikolay37731 <anikolai3773@gmail.com>
2026-01-11 23:16:31 +0100

    Solved problem: Fast food

---
+++ StacksAndQueues/12_fastFood/Program.cs
@@ -0,0 +1,30 @@
+﻿int totalFood = int.Parse(Console.ReadLine());
+Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+Console.WriteLine(orders.Max());
+
+while (true)
+{
+    int currentOrder = orders.Peek();
+
+    if (totalFood - currentOrder >= 0)
+    {
+        totalFood -= currentOrder;
+        orders.Dequeue();
+        if (!orders.Any())
+        {
+           Console.WriteLine("Orders complete");
+           break;
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Orders left: {String.Join(" ", orders)}");
+        break;
+    }
+
+    if (!orders.Any())
+    {
+        Console.WriteLine("Orders complete");
+        break;
+    }
+}
