98765ef4ced7c2b047c19877d3b4f90050ae07a9
Nikolay37731 <anikolai3773@gmail.com>
2026-01-18 09:50:29 +0100

    Solved problem: Fashion boutique

---
+++ StacksAndQueues/13_fashionBoutique/Program.cs
@@ -0,0 +1,23 @@
+﻿var clothing = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var stack = new Stack<int>(clothing);
+
+var capacity = int.Parse(Console.ReadLine());
+var sum = 0;
+var numberOfRacks = 1;
+
+while (stack.Any())
+{
+    int currentCloth = stack.Pop();
+
+    if (sum + currentCloth <= capacity)
+    {
+        sum += currentCloth;
+    }
+    else
+    {
+        numberOfRacks++;
+        sum = currentCloth;
+    }
+}
+
+Console.WriteLine(numberOfRacks);
