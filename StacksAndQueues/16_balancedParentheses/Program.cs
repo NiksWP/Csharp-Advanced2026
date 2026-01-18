a5f4e4d4d620328ab74143c46a1d5796aecf6856
Nikolay37731 <anikolai3773@gmail.com>
2026-01-18 13:41:39 +0100

    Solved problem: Balanced parentheses

---
+++ StacksAndQueues/16_balancedParentheses/Program.cs
@@ -0,0 +1,29 @@
+﻿var input = Console.ReadLine();
+var stack = new Stack<char>(input.Substring(input.Length / 2));
+var queue = new Queue<char>(input.Substring(0, input.Length - stack.Count));
+
+var invalid = false;
+
+for (int i = 0; i < queue.Count; i++)
+{
+    var currentQueueElement = queue.Dequeue();
+    var currentStackElement = stack.Pop();
+
+    if (currentQueueElement == '(' && currentStackElement == ')'
+    || currentQueueElement == '{' && currentStackElement == '}'
+    || currentQueueElement == '[' && currentStackElement == ']')
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("NO");
+        invalid = true;
+        break;
+    }
+}
+
+if (!invalid)
+{
+        Console.WriteLine("YES");
+}
