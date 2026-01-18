b4efb514db306ea22ca29047cd904246b01f6c3d
Nikolay37731 <anikolai3773@gmail.com>
2026-01-18 10:39:21 +0100

    Solved problem: Songs queue

---
+++ StacksAndQueues/14_songsQueue/Program.cs
@@ -0,0 +1,33 @@
+﻿var queue = new Queue<string>(Console.ReadLine().Split(", ").ToArray());
+
+while (queue.Any())
+{
+ string[] input = Console.ReadLine().Split(" ", 2);
+ string command = input[0];
+ string song = input.Length > 1 ? input[1] : null;
+
+    if (command == "Play")
+    {
+        queue.Dequeue();
+        if (!queue.Any())
+        {
+            Console.WriteLine("No more songs!");
+            break;
+        }
+    }
+    else if (command == "Add")
+    {
+        if (!queue.Contains(song))
+        {
+            queue.Enqueue(song);
+        }
+        else
+        {
+            Console.WriteLine($"{song} is already contained!");
+        }
+    }
+    else if (input[0] == "Show")
+    {
+        Console.WriteLine(String.Join(", ", queue));
+    }
+}
