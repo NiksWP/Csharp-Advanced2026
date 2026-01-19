e8c9e7e31d1185a347c092dd46bb1b6ae44c5dce
Nikolay37731 <anikolai3773@gmail.com>
2026-01-19 18:22:32 +0100

    Solved problem: Crossroads

---
+++ StacksAndQueues/18_crossroads/Program.cs
@@ -0,0 +1,73 @@
+﻿var value = int.Parse(Console.ReadLine());
+var window = int.Parse(Console.ReadLine());
+int passed = 0;
+
+var queue = new Queue<string>();
+
+// 10
+// 5
+
+
+// Mercedes
+// Hummer
+// green
+// Hummer
+// Mercedes
+// green
+// END
+
+string input;
+while ((input = Console.ReadLine()) != "END")
+{
+    if (input == "green")
+    {
+        int green = value;
+        if (!queue.Any())
+        {
+            continue;
+        }
+
+
+
+        while (queue.Count > 0 && green > 0)
+        {
+            var car = queue.Dequeue();
+            if (car.Length == green)
+            {
+                passed++;
+                green = 0;
+            }
+            else if (car.Length < green)
+            {
+                passed++;
+                green -= car.Length;
+            }
+            else if (car.Length > green)
+            {
+                if (car.Length == green + window)
+                {
+                    passed++;
+                    green = 0;
+                }
+                else if (car.Length < green + window)
+                {
+                    passed++;
+                    green = 0;
+                }
+                else if (car.Length > green + window)
+                {
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{car} was hit at {car[green + window]}.");
+                    return;
+                }
+            }
+        }
+    }
+    else
+    {
+        queue.Enqueue(input);
+    }
+}
+
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{passed} total cars passed the crossroads.");
