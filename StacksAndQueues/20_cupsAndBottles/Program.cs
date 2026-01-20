f9acc0e70895134397c05afb2c0d19c3a4e5180d
Nikolay37731 <anikolai3773@gmail.com>
2026-01-20 08:11:39 +0100

    Solver problem: Cups and bottles

---
+++ StacksAndQueues/20_cupsAndBottles/Program.cs
@@ -0,0 +1,42 @@
+﻿var cups = new Queue<int>(Console.ReadLine()
+.Split().Select(int.Parse).ToArray());
+var bottles = new Stack<int>(Console.ReadLine()
+.Split().Select(int.Parse).ToArray());
+var wastedWater = 0;
+var remainingCup = 0;
+
+while (true)
+{
+    var cup = cups.Dequeue();
+
+    while (cup > 0)
+    {
+        var bottle = bottles.Pop();
+        if (bottle - cup == 0)
+        {
+            break;
+        }
+        else if (bottle > cup)
+        {
+            wastedWater += bottle - cup;
+            break;
+        }
+        else
+        {
+            cup -= bottle;
+        }
+    }
+
+    if (!cups.Any())
+    {
+        Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+        Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        break;
+    }
+    else if (!bottles.Any())
+    {
+        Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+        Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        break;
+    }
+}
