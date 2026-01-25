3e3ead4ad4dd0f05b6958e9bc09caacb2299901f
Nikolay37731 <anikolai3773@gmail.com>
2026-01-25 07:34:41 +0100

    Solved problem: Sum matrix elements

---
+++ MultidiamentionalArrays/01_sumMatrixElements/Program.cs
@@ -0,0 +1,27 @@
+﻿var dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+int[,] matrix = new int[dimensions[0], dimensions[1]];
+var sum = 0;
+
+for (int rows = 0; rows < dimensions[0]; rows++)
+{
+    var col = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+    for (int cols = 0; cols < dimensions[1]; cols++)
+    {
+        matrix[rows, cols] = col[cols];
+        sum += matrix[rows,cols];
+    }
+}
+
+Console.WriteLine(matrix.GetLength(0));
+Console.WriteLine(matrix.GetLength(1));
+Console.WriteLine(sum);
+
+// for (int row = 0; row < matrix.GetLength(0); row++)
+// {
+//     for (int col = 0; col < matrix.GetLength(1); col++)
+//     {
+//         Console.Write(matrix[row,col] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
