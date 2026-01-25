16fa911fbe430d502f4e7623d5996eab34dd77d2
Nikolay37731 <anikolai3773@gmail.com>
2026-01-25 07:59:17 +0100

    Solved problem: Primary diagonal

---
+++ MultidiamentionalArrays/03_primaryDiagonal/Program.cs
@@ -0,0 +1,19 @@
+﻿var size = int.Parse(Console.ReadLine());
+int[,] matrix = new int[size, size];
+var sum = 0;
+
+for (int row = 0; row < size; row++)
+{
+    var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    for (int col = 0; col < size; col++)
+    {
+        matrix[row,col] = input[col];
+    }
+}
+
+for (int i = 0; i < size; i++)
+{
+    sum += matrix[i, i];
+}
+
+Console.WriteLine(sum);
