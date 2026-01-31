5ccb216c261a9aa0f569053bf7aa98241f16fa26
Nikolay37731 <anikolai3773@gmail.com>
2026-01-31 12:58:55 +0100

    Solved problem: Squares in matrix

---
+++ MultidiamentionalArrays/09_squaresInMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿var size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+char[,] matrix = new char[size[0], size[1]];
+var count = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    string input = Console.ReadLine().Replace(" ", "");
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = input[col];
+    }
+}
+
+for (int row = 1; row < matrix.GetLength(0); row++)
+{
+    for (int col = 1; col < matrix.GetLength(1); col++)
+    {
+        var value = matrix[row, col];
+        if (value == matrix[row, col -1] && value == matrix[row - 1, col -1]
+            && value == matrix[row - 1, col])
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine(count);
