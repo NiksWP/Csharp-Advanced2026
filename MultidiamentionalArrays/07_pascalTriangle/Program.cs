39e3a593a181f94a6f0552f5df1f91fe86bd6352
Nikolay37731 <anikolai3773@gmail.com>
2026-01-31 08:20:28 +0100

    Solved problem: Pascal triangle

---
+++ MultidiamentionalArrays/07_pascalTriangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+var size = int.Parse(Console.ReadLine());
+BigInteger[][] matrix = new BigInteger[size][];
+
+
+if (size == 1)
+{
+    matrix[0] = new BigInteger[1]{1};
+    Console.WriteLine(1);
+    return;
+}
+else if(size == 0)
+{
+    return;
+}
+
+matrix[0] = new BigInteger[1]{1};
+matrix[1] = new BigInteger[2]{1, 1};
+
+for (int row = 2; row < size; row++)
+{
+    matrix[row] = new BigInteger[row + 1];
+    
+    for (int col = 0; col < row + 1; col++)
+    {
+        if (col == 0 || col == row)
+        {
+            matrix[row][col] = 1;
+            continue;
+        }
+        matrix[row][col] = matrix[row - 1][col] + matrix[row - 1][col - 1];
+    }
+}
+
+for (int row = 0; row < size; row++)
+{
+    Console.WriteLine(string.Join(" ", matrix[row]));
+}
