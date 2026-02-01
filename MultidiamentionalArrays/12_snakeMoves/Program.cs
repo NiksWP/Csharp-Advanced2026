e0d7d85bec744fa54b909c5bbc9df2cd024333e4
Nikolay37731 <anikolai3773@gmail.com>
2026-02-01 07:19:22 +0100

    Solved problem: Snake moves

---
+++ MultidiamentionalArrays/12_snakeMoves/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design.Serialization;
+using Microsoft.VisualBasic;
+
+var size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+char[,] matrix = new char[size[0], size[1]];
+var queue = new Queue<char>(Console.ReadLine());
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    if (row % 2 == 0)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            queue.Enqueue(queue.Peek());
+            matrix[row, col] = queue.Dequeue();
+        }
+    }
+    else
+    {
+        for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+        {
+            queue.Enqueue(queue.Peek());
+            matrix[row,col] = queue.Dequeue();
+        }
+    }
+}
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        Console.Write(matrix[row,col]);
+    }
+    Console.WriteLine();
+}
