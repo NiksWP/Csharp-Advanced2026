91390581b472e65ee4de6114d06cb3d115e87ba6
Nikolay37731 <anikolai3773@gmail.com>
2026-02-06 08:50:59 +0100

    Solved problem: Average student grades

---
+++ SetsAndDictionaries/02_averageStudentGrades/Program.cs
@@ -0,0 +1,20 @@
+﻿var n = int.Parse(Console.ReadLine());
+var grades = new Dictionary<string,List<double>>();
+
+for (int i = 0; i < n; i++)
+{
+    var input = Console.ReadLine().Split().ToArray();
+    var name = input[0];
+    var grade = double.Parse(input[1]);
+
+    if (!grades.ContainsKey(name))
+    {
+        grades.Add(name, new List<double>());
+    }
+    grades[name].Add(grade);
+}
+
+foreach (var student in grades)
+{
+    Console.WriteLine($"{student.Key} -> {string.Join(" ", (student.Value.Select(x => x.ToString("F2"))))} (avg: {student.Value.Average():F2})");
+}
