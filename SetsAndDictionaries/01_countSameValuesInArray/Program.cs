19cafa3e3fa2b9d6e2130153c667f09458c93f0b
Nikolay37731 <anikolai3773@gmail.com>
2026-02-06 08:26:17 +0100

    Solved problem: countSameValuesInArray

---
+++ SetsAndDictionaries/01_countSameValuesInArray/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+var dictionary = new Dictionary<double, int>();
+
+foreach (var number in input)
+{
+    if (!dictionary.Keys.Contains(number))
+    {
+        dictionary.Add(number, 1);
+        continue;
+    }
+    dictionary[number]++;
+}
+
+foreach (var entry in dictionary)
+{
+    Console.WriteLine(entry.Key + " - " + entry.Value + " times");
+}
