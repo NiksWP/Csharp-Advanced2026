491109d985f8c9561669cd1b900e162448d9df16
Nikolay37731 <anikolai3773@gmail.com>
2026-02-07 10:11:50 +0100

    Solved problem: Softuni party

---
+++ SetsAndDictionaries/08_softUniParty/Program.cs
@@ -0,0 +1,41 @@
+﻿var vip = new HashSet<string>();
+var regular = new HashSet<string>();
+
+string guests;
+while ((guests = Console.ReadLine()) != "PARTY")
+{
+    if (char.IsDigit(guests[0]))
+    {
+        vip.Add(guests);
+    }
+    else
+    {
+        regular.Add(guests);
+    }
+}
+
+var count = vip.Count + regular.Count;
+
+string input;
+while ((input = Console.ReadLine()) != "END")
+{
+    if (char.IsDigit(input[0]))
+    {
+        vip.Remove(input);
+    }
+    else
+    {
+        regular.Remove(input);
+    }
+}
+
+Console.WriteLine(vip.Count + regular.Count);
+foreach (var guest in vip)
+{
+    Console.WriteLine(guest);
+}
+
+foreach (var guest in regular)
+{
+    Console.WriteLine(guest);
+}
