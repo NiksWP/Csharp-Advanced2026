5eec2157cd65a49a1d97fab9913dc6808b956220
Nikolay37731 <anikolai3773@gmail.com>
2026-02-07 09:06:29 +0100

    Solved problem: Product shop

---
+++ SetsAndDictionaries/06_productShop/Program.cs
@@ -0,0 +1,26 @@
+﻿var shops = new Dictionary<string, Dictionary<string, double>>();
+
+string input;
+while ((input = Console.ReadLine()) != "Revision")
+{
+    var tokens = input.Split(", ").ToArray();
+    var shop = tokens[0];
+    var product = tokens[1];
+    var price = double.Parse(tokens[2]);
+
+    if (!shops.ContainsKey(shop))
+    {
+        shops[shop] = new Dictionary<string, double>();
+    }
+
+    shops[shop].Add(product, price);
+}
+
+foreach (var shop in shops.OrderBy(x => x.Key))
+{
+    Console.WriteLine(shop.Key + "->");
+    foreach (var kvp in shop.Value)
+    {
+        Console.WriteLine($"Product: {kvp.Key}, Price: {kvp.Value}");
+    }
+}
