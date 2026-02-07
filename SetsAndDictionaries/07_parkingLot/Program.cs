acdf03d6c07d8514c6bdd7c26db8226625fdd9bd
Nikolay37731 <anikolai3773@gmail.com>
2026-02-07 09:47:49 +0100

    Solved problem: Parking lot

---
+++ SetsAndDictionaries/07_parkingLot/Program.cs
@@ -0,0 +1,34 @@
+﻿var parking = new HashSet<string>();
+
+var empty = false;
+string input;
+while ((input = Console.ReadLine()) != "END")
+{
+    var tokens = input.Split(", ").ToArray();
+    var car = tokens[1];
+
+    if (tokens[0] == "IN")
+    {
+        parking.Add(car);
+    }
+    else if (tokens[0] == "OUT")
+    {
+        if (!parking.Any())
+        {
+            empty = true;
+            break;
+        }
+        parking.Remove(car);
+    }
+    else continue;
+}
+
+if (empty == false && parking.Any())
+{
+    Console.WriteLine(String.Join("\n", parking));
+    return;
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
