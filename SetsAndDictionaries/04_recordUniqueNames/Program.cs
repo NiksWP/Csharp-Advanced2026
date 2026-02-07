69147231f2f3bcb6e225e5aa91c13cb8313a7599
Nikolay37731 <anikolai3773@gmail.com>
2026-02-07 09:06:13 +0100

    Solved problem: Record unique names

---
+++ SetsAndDictionaries/04_recordUniqueNames/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+var hashset = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    var name = Console.ReadLine();
+
+    if (!hashset.Contains(name))
+    {
+        hashset.Add(name);
+    }
+}
+
+Console.WriteLine();
+
+foreach (var name in hashset)
+{
+    Console.WriteLine(name);
+}
