e2f615f77fd22d8c8e866813d15adf8fce2f77d9
Nikolay37731 <anikolai3773@gmail.com>
2026-02-08 14:16:25 +0100

    Solved problem: Unique usernames

---
+++ SetsAndDictionaries/09_uniqueUsernames/Program.cs
@@ -0,0 +1,10 @@
+﻿var n = int.Parse(Console.ReadLine());
+var names = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    var name = Console.ReadLine();
+    names.Add(name);
+}
+
+Console.WriteLine(String.Join("\n", names));
