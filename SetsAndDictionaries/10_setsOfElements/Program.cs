3b349a8cd528caaf08b5a6e604a45248a2e8fa7f
Nikolay37731 <anikolai3773@gmail.com>
2026-02-08 14:29:59 +0100

    Solved problem: Sets of elements

---
+++ SetsAndDictionaries/10_setsOfElements/Program.cs
@@ -0,0 +1,28 @@
+﻿var lengths = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = lengths[0], m = lengths[1];
+
+var nSet = new HashSet<int>();
+var mSet = new HashSet<int>();
+var list = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    var num = int.Parse(Console.ReadLine());
+    nSet.Add(num);
+}
+
+for (int i = 0; i < m; i++)
+{
+    var num = int.Parse(Console.ReadLine());
+    mSet.Add(num);
+}
+
+foreach (var num in nSet)
+{
+    if (mSet.Contains(num))
+    {
+        list.Add(num);
+    }
+}
+
+Console.WriteLine(string.Join(" ", list));
