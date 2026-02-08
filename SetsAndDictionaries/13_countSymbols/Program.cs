cde570586ac27935dffdda75e71428f1bef0ad3e
Nikolay37731 <anikolai3773@gmail.com>
2026-02-08 15:02:50 +0100

    Solved problem: Count symbols

---
+++ SetsAndDictionaries/13_countSymbols/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = Console.ReadLine();
+var symbols = new Dictionary<char, int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    var current = input[i];
+    if (!symbols.ContainsKey(current))
+    {
+        symbols[current] = 0;
+    }
+    symbols[current]++;
+}
+
+foreach (var kvp in symbols.OrderBy(x => x.Key))
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+}
