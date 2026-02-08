50a21757ae4b7ac965160b3dac864c6e892035ca
Nikolay37731 <anikolai3773@gmail.com>
2026-02-08 14:37:38 +0100

    Solved problem: Periodic table

---
+++ SetsAndDictionaries/11_periodicTable/Program.cs
@@ -0,0 +1,15 @@
+﻿var n = int.Parse(Console.ReadLine());
+var set = new SortedSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    var compounds = Console.ReadLine().Split().ToArray();
+
+    for (int j = 0; j < compounds.Length; j++)
+    {
+     var current = compounds[j];
+     set.Add(current);
+    }
+}
+
+Console.WriteLine(string.Join(" ", set));
