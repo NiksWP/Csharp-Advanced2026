029efdb78aef1b3f739c949971adf26c7668a094
Nikolay37731 <anikolai3773@gmail.com>
2026-02-08 14:51:14 +0100

    Solved problem: Even times

---
+++ SetsAndDictionaries/12_evenTimes/Program.cs
@@ -0,0 +1,15 @@
+﻿var n = int.Parse(Console.ReadLine());
+var numbers = new Dictionary<int, int>();
+
+for (int i = 0; i < n; i++)
+{
+    var current = int.Parse(Console.ReadLine());
+    if (!numbers.ContainsKey(current))
+    {
+        numbers[current] = 0;
+    }
+    numbers[current]++;
+}
+
+var target = numbers.Where(x => x.Value % 2 == 0).FirstOrDefault();
+Console.WriteLine(target.Key);
