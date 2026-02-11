e3f444141842f8577ae7c13226f32fb0894237e5
Nikolay37731 <anikolai3773@gmail.com>
2026-02-11 11:18:39 +0100

    Solved problem: Line numbers

---
+++ StreamsFilesAndDirectories/02_lineNumbers/LineNumbers.cs
@@ -0,0 +1,33 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using var reader = new StreamReader(inputFilePath);
+            using var writer = new StreamWriter(outputFilePath);
+
+            int count = 1;
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null) break;
+
+                writer.WriteLine($"{count}. {line}");
+                count++;
+            }
+        }
+    }
+}
