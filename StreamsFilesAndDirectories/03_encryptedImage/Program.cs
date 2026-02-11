ec3dbfd0afab376e17c18f02cb7dcc4d46e82f55
Nikolay37731 <anikolai3773@gmail.com>
2026-02-11 12:51:38 +0100

    Solved problem: Encrypted image

---
+++ StreamsFilesAndDirectories/03_encryptedImage/Program.cs
@@ -0,0 +1,9 @@
+﻿var byteContents = File.ReadAllBytes(@"Files/cat-encrypted.webp");
+var password = 187;
+
+for (int i = 0; i < byteContents.Length; i++)
+{
+    byteContents[i] = (byte) (password ^ byteContents[i]);
+}
+
+File.WriteAllBytes(@"Files/cat-decrypted.webp", byteContents);
