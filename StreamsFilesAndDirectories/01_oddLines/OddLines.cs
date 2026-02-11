03866c977c36c21e42840ae852a4a603739a6cb9
Nikolay37731 <anikolai3773@gmail.com>
2026-02-11 10:13:10 +0100

    Solved problem: Odd lines

---
+++ StreamsFilesAndDirectories/01_oddLines/OddLines.cs
@@ -0,0 +1,37 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            //string inputFilePath = @"..\..\..\Files\input.txt";
+            //string outputFilePath = @"..\..\..\Files\output.txt";
+            string inputFilePath = "Files/input.txt";
+            string outputFilePath = "Files/output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using var reader = new StreamReader(inputFilePath);
+            using var writer = new StreamWriter(outputFilePath, true);
+
+            var counter = 0;
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null) break;
+
+                if (counter % 2 == 1)
+                {
+                    writer.WriteLine(line);
+                }
+
+                counter++;
+            }
+        }
+    }
+}
