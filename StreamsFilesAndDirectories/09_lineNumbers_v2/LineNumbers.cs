a9cf0fddca4f8691ac4df8271db0deb8866463f5
Nikolay37731 <anikolai3773@gmail.com>
2026-02-13 14:37:11 +0100

    Solved problem: Line numbers version 2

---
+++ StreamsFilesAndDirectories/09_lineNumbers_v2/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"text.txt";
+            string outputFilePath = @"output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            var lines = File.ReadAllLines(inputFilePath);
+            var list = new List<string>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                list.Add($"Line {i + 1}: {lines[i]} ({lines[i].Where(x => char.IsLetter(x)).Count()})({lines[i].Where(x => char.IsPunctuation(x)).Count()})");
+            }
+
+            using var writer = new StreamWriter(outputFilePath);
+            foreach (var sentance  in list)
+            {
+                writer.WriteLine(sentance);
+            }
+        }
+    }
+}
