0aa26490f265467fad9a49a003aa89d863b9d750
Nikolay37731 <anikolai3773@gmail.com>
2026-02-13 15:23:02 +0100

    Solved problem: Copy binary file

---
+++ StreamsFilesAndDirectories/10_copyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"copyMe.png";
+            string outputFilePath = @"copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using var inputFile = new FileStream(inputFilePath, FileMode.Open);
+            using var outputFile = new FileStream(outputFilePath, FileMode.Create);
+
+            var buffer = new byte[4096];
+            int bufferLength;
+
+            while ((bufferLength = inputFile.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                outputFile.Write(buffer, 0, bufferLength);
+            }
+        }
+    }
+}
