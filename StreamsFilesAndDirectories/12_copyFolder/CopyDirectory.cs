73e074ce58e26264f179422049e5828388bf4dd1
Nikolay37731 <anikolai3773@gmail.com>
2026-02-15 13:38:56 +0100

    Solved problem: Copy directory

---
+++ StreamsFilesAndDirectories/12_copyFolder/CopyDirectory.cs
@@ -0,0 +1,34 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath =  @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, recursive: true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            foreach (var file in Directory.EnumerateFiles(inputPath))
+            {
+                string name = Path.GetFileName(file);
+                string fullName = Path.Combine(outputPath, name);
+
+                File.Copy(file, fullName);
+            }
+        }
+    }
+}
