45dbf63d50704f6db0fa42b01acad19ce933f92c
Nikolay37731 <anikolai3773@gmail.com>
2026-02-16 21:54:36 +0100

    Solved problem: Sort even numbers

---
+++ FunctionalProgramming/01_sortEvenNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿var input = Console.ReadLine()
+        .Split(", ")
+            .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                    .ToArray();
+
+Console.WriteLine(string.Join(", ", input.OrderBy(x => x)));
+
