32fbdbb52c58d4d09e3818859909925237093dc1
Nikolay37731 <anikolai3773@gmail.com>
2026-03-09 16:32:50 +0100

    Solved problem: Car constructors

---
+++ DefiningClasses/03_carConstructors/car.cs
@@ -0,0 +1,120 @@
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 220;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new Exception("invalid input");
+                }
+
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new Exception("invalid input");
+                }
+
+                model = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("invalid input");
+                }
+
+                year = value;
+            }
+        }
+
+        public double FuelQuantity
+        {
+            get;
+            set;
+        }
+
+        public double FuelConsumption
+        {
+            get;
+            set;
+        }
+
+       public string Drive(double distance)
+        {
+            if (this.fuelQuantity - (distance * this.fuelConsumption) > 0)
+            {
+                return "Done";
+            }
+
+            return "Not enough fuel to perform the trip!";
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:F2}");
+
+            return sb.ToString();
+        }
+    }
+}
