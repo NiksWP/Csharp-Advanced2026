5de4ec49b1b8b9f732479045df53278864a64e2f
Nikolay37731 <anikolai3773@gmail.com>
2026-03-11 14:51:45 +0100

    Solved problem: Define class Person

---
+++ DefiningClasses/06_definaClassPerson/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Person person = new Person();
+            person.Name = "niki";
+            person.Age = 21;
+        }
+    }
+}
