172b7ae51b3f0298aff54ed9715cd1c5565786c8
Nikolay37731 <anikolai3773@gmail.com>
2026-03-11 15:55:32 +0100

    Solved problem: Oldest family member

---
+++ DefiningClasses/08_oldestFamilyMember/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Family family = new Family();
+
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var name = input[0];
+                var age = int.Parse(input[1]);
+                
+                family.AddMembers(new Person(name, age));
+            }
+
+            Console.WriteLine(family.GetOldestPerson());
+        }
+    }
+}
